40423e51520563a2be248ae14f769f6ef2d58367
Gabriel Ruiz <gruiz@affinipay.com>
2022-11-16 21:53:40 -0700

    calculadora app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'caluladora-app';
+
+  operandoA = 0;
+  operandoB = 0;
+  resultado = 0;
+
+  calcular(event: Event): void  {
+    let element = (event.target as Element);
+    switch (element.closest('button')?.id) {
+      case 'sumar':
+        this.resultado = this.operandoA + this.operandoB;
+        break;
+      case 'restar':
+        this.resultado = this.operandoA - this.operandoB;
+        break;
+      case 'multiplicar':
+        this.resultado = this.operandoA * this.operandoB;
+        break;
+      case 'dividir':
+        this.resultado = this.operandoA / this.operandoB;
+        break;
+    }
+  }
 }
